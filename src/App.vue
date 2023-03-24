827e85c518eeeebe9aced5f8ff75ec20488a66a3
Zhang-jiang-zhan <654884102@qq.com>
2023-03-25 01:21:51 +0800

    项目搭建+antdv引入+iconfont引入

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+*{
+  padding:0;
+  margin:0;
+  box-sizing: border-box;
+}
+@import "assets/css/iconfont.css";
+</style>
