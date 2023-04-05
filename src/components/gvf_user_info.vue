d9a6bf544b848363a3a5a68f0bab6e53247d9279
Zhang-jiang-zhan <654884102@qq.com>
2023-04-05 16:44:41 +0800

    顶部用户模块化

---
+++ src/components/gvf_user_info.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="gvf_user_info">
+    <div class="avatar">
+      <img src="https://img95.699pic.com/element/40151/6832.png_300.png" alt="">
+    </div>
+    <div class="drop_menu">
+      <a-dropdown placement="bottomRight">
+        <a class="ant-dropdown-link" @click.prevent>
+          江湛
+          <i class="fa fa-angle-down"></i>
+        </a>
+        <template #overlay>
+          <a-menu @click="menuClick">
+            <a-menu-item key="user_center">
+              <a href="javascript:">个人中心</a>
+            </a-menu-item>
+            <a-menu-item key="file_store">
+              <a href="javascript:">我的仓库</a>
+            </a-menu-item>
+            <a-menu-item key="logout">
+              <a href="javascript:">退出登录</a>
+            </a-menu-item>
+          </a-menu>
+        </template>
+      </a-dropdown>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import {useRouter} from "vue-router/dist/vue-router";
+
+const router = useRouter()
+
+function menuClick({key}) {
+  if (key === "logout") {
+    console.log("logout")
+    return
+  }
+  router.push({
+    name: key
+  })
+}
+</script>
+
+
+<style>
+.gvf_user_info {
+  display: flex;
+  align-items: center;
+}
+</style>
--- src/views/admin/admin.vue
+++ src/views/admin/admin.vue
@@ -16,30 +16,7 @@
             <GVFTheme></GVFTheme>
             <GVFFullScreen></GVFFullScreen>
           </div>
-          <div class="avatar">
-            <img src="https://img95.699pic.com/element/40151/6832.png_300.png" alt="">
-          </div>
-          <div class="drop_menu">
-            <a-dropdown placement="bottomRight">
-              <a class="ant-dropdown-link" @click.prevent>
-                江湛
-                <i class="fa fa-angle-down"></i>
-              </a>
-              <template #overlay>
-                <a-menu @click="menuClick">
-                  <a-menu-item key="user_center">
-                    <a href="javascript:">个人中心</a>
-                  </a-menu-item>
-                  <a-menu-item key="file_store">
-                    <a href="javascript:">我的仓库</a>
-                  </a-menu-item>
-                  <a-menu-item key="logout">
-                    <a href="javascript:">退出登录</a>
-                  </a-menu-item>
-                </a-menu>
-              </template>
-            </a-dropdown>
-          </div>
+          <GVFUserInfo></GVFUserInfo>
         </div>
       </header>
       <div class="tabs"></div>
@@ -53,22 +30,7 @@
 import GVFTheme from "../../components/gvf_theme.vue"
 import GVFAside from "../../components/admin/gvf_aside.vue"
 import GVFFullScreen from "../../components/gvf_full_screen.vue"
-import {useRouter} from "vue-router"
-import {ref} from "vue";
-
-const router = useRouter()
-
-
-function menuClick({key}) {
-  if (key === "logout") {
-    console.log("logout")
-    return
-  }
-  router.push({
-    name: key
-  })
-}
-
+import GVFUserInfo from "../../components/gvf_user_info.vue"
 
 </script>
 
